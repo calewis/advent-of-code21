bc9049957f723002cf54da576758c24836f613ce
Drew <drew90@vt.edu>
2021-12-01 21:14:23 -0500

    Day 1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -11,7 +11,19 @@ fn main() -> io::Result<()> {
         data.push(value);
     }
 
-    let sum = &data
+    //
+    // The following is for day 1 part 1
+    //
+    // let sum = &data
+    //     .windows(2)
+    //     .map(|w| if w[1] > w[0] { 1 } else { 0 })
+    //     .sum::<i32>();
+
+    let windows = &data
+        .windows(3)
+        .map(|w| w.iter().sum())
+        .collect::<Vec<i32>>();
+    let sum = &windows
         .windows(2)
         .map(|w| if w[1] > w[0] { 1 } else { 0 })
         .sum::<i32>();
