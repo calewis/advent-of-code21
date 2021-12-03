fa5de4fb5519acaaec273a50011cd5d5155b7eec
Drew <drew90@vt.edu>
2021-12-03 09:55:02 -0500

    Day 3 finished part 2 in rust

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -7,9 +7,9 @@ fn main() {
     let marjority_threshold = input.len() / 2;
 
     let mut count: [usize; 12] = [0; 12];
-    for num in input {
+    for num in input.iter() {
         for i in 0..12 {
-            count[i] += if num & (1<<i) > 0 {1} else {0}; 
+            count[i] += if num & (1 << i) > 0 { 1 } else { 0 };
         }
     }
 
@@ -19,6 +19,46 @@ fn main() {
         gamma += signal[i] << i;
     }
     let epsilon = gamma ^ 0xFFF;
-    println!("Power: {}", epsilon * gamma);
+    println!("Part 1 Power: {}", epsilon * gamma);
 
+    //
+    // Part 2
+    //
+
+    // Oxygen First
+    let mut oxygen: Vec<u16> = input.clone();
+    let mut o2reading: usize = 0;
+    for i in 0..12 {
+        let (ones, zeros): (Vec<u16>, Vec<u16>) =
+            oxygen.iter().partition(|n| (*n & (1 << (11 - i))) > 0);
+        if ones.len() >= zeros.len() {
+            oxygen = ones;
+        } else {
+            oxygen = zeros;
+        }
+        if oxygen.len() == 1 {
+            o2reading = oxygen[0] as usize;
+            break;
+        }
+    }
+
+    // CO2 Second
+    let mut co2: Vec<u16> = input.clone();
+    let mut co2_reading: usize = 0;
+    for i in 0..12 {
+        let (ones, zeros): (Vec<u16>, Vec<u16>) =
+            co2.iter().partition(|n| (*n & (1 << (11 - i))) > 0);
+        if zeros.len() =< ones.len() {
+            co2 = zeros;
+        } else {
+            co2 = ones;
+        }
+        if co2.len() == 1 {
+            co2_reading = co2[0] as usize;
+            break;
+        }
+    }
+
+    println!("o2: {}, cO2: {}", o2reading, co2_reading);
+    println!("Part 2: {}", o2reading * co2_reading);
 }
